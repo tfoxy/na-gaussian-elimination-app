49bb8956624151f47a910640108d983f31c7396c
Tomás Fox <tomas.c.fox@gmail.com>
2015-10-06 12:43:36 -0300

    Add some simple protractor tests

--- e2e/main.po.js
+++ e2e/main.po.js
@@ -2,13 +2,23 @@
  * This file uses the Page Object pattern to define the main page for tests
  * https://docs.google.com/presentation/d/1B6manhG0zEXkC-H-tPo2vwU06JhL8w9-XCF9oehXzAQ
  */
-/*
-var MainPage = function() {
-  this.jumbEl = element(by.css('.jumbotron'));
-  this.h1El = this.jumbEl.element(by.css('h1'));
-  this.imgEl = this.jumbEl.element(by.css('img'));
-  this.thumbnailEls = element(by.css('body')).all(by.repeater('awesomeThing in main.awesomeThings'));
-};
+
+function MainPage() {
+  this.form = $('form[name="inputForm"]');
+  this.solutionView = $('#solution-view');
+  this.stepsView = $('#steps-view');
+
+  this.matrixTable = this.form.element(by.css('matrix-table'));
+  this.precisionInput = this.form.element(by.css('input[name="decimalPrecisionNumber"]'));
+  this.exactTypeRadio = this.form.element(by.css('input[name="exactNumberType"]'));
+  this.decimalTypeRadio = this.form.element(by.css('input[name="decimalNumberType"]'));
+  this.pivotingSelect = this.form.element(by.css('select[name="pivoting"]'));
+  this.matrixInput = this.form.element(by.css('matrix-input'));
+  this.roundingModeSelect = this.form.element(by.css('select[name="roundingMode"]'));
+  this.zerosRadio = this.form.element(by.css('select[name="luFlagFalse"]'));
+  this.luRadio = this.form.element(by.css('select[name="luFlagTrue"]'));
+
+  this.submitButton = this.form.element(by.css('button[type="submit"]'));
+}
 
 module.exports = new MainPage();
-*/
--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -1,21 +1,21 @@
-/*
 describe('The main view', function() {
-  let page;
+  var page;
 
   beforeEach(function() {
     browser.get('/index.html');
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('should include a form', function() {
+    expect(page.form.isPresent()).toBe(true);
   });
 
-  it('should list more than 5 awesome things', function() {
-    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
+  describe('form', function() {
+
+    it('should include a pivoting select element', function() {
+      expect(page.pivotingSelect.isPresent()).toBe(true);
+    });
+
   });
 
 });
-*/
